8d738405de0a2149b7a2d77333824f7f9bb3853d
alkasdci <alkas.alkiviadis-freelancer@digitalcareerinstitute.org>
2023-05-19 21:46:07 +0300

    setup for vercel

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
-import db from "./config/db.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import postRoutes from "./src/routes/postRoutes.js";
+import db from "./src/config/db.js";
+import cors from "cors";
 // import cookieParser from 'cookie-parser'
 // import Post from "./models/Post.js";
 
@@ -14,6 +15,14 @@ db();
 app.use(express.json());
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    preflightContinue: true,
+  })
+);
 
 const port = process.env.PORT || 4001;
 app.listen(port, () => console.log("Server is up and running at port", port));
